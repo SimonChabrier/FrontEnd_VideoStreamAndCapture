ea931df4e662fdfda99fa6dce8a037f4337856bb
Simon Chabrier <simonchabrier@MacBook-Pro-de-Simon.local>
2022-04-14 19:52:42 +0200

    Stable V1.2

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,31 +8,30 @@ const app = {
     
     //lister tous le spérifériues de capture
     listDevice:function (){
-        //je récupère ma balise select
         let select = document.getElementById('select');
-        // je liste les devices vidéo et audio diponnibles sur mon péréphérique
+        // je récupère les devices vidéo et audio diponnibles sur mon péréphérique
         navigator.mediaDevices.enumerateDevices().then(function(devices) {
-            //je boucle sur chaque device dispo
+            //je boucle sur chaque péréfériques audio et vidéo existants
             devices.forEach(function(device) {
                 //si ce sont des device de type vidéo alor je les ajoutent dans les options de mon select
                 if (device.kind === 'videoinput') 
                 {
                     const option = document.createElement('option');
                     option.nodeType = 'submit';
-                    //ici j'ai comme value de mes selects le nom des péréfériques dispo
+                    //je passe aux value de mon select le nom des péréfériques dispo
                     option.value = device.deviceId;
                     const label = device.label || `Camera ${count++}`;
                     const textNode = document.createTextNode(label);
                     option.appendChild(textNode);
                     select.appendChild(option);
                 }
-                // ici j'ai la liste de tout mes péréphériques
-                    //console.log(device.kind + ": " + device.label + " id = " + device.deviceId);   
+                    //la liste de mes péréphériques
+                    console.log(device.kind + ": " + device.label + " id = " + device.deviceId);   
             });
         })
 
         .catch(function(err) {
-            console.log('je suis dans cette erreur' + err.name + ": " + err.message);
+            app.dislayError(' Erreur dans la listDevice ' + err.name + ": " + err.message);
         });  
     },
     
@@ -45,7 +44,6 @@ const app = {
         //* variables 'globales'
         let video = document.querySelector('video')
         let constrainsList = document.getElementById('constraintList')
-        let start = document.getElementById('start');
         let stop = document.getElementById('stop');
         let catchButton = document.getElementById('catch');
         let clearButton = document.getElementById('reset');
@@ -59,7 +57,7 @@ const app = {
         clearButton.classList.add('hidden');
         canvas.classList.add('hidden');
 
-    
+        let start = document.getElementById('start');
         //* lancement du stream au click sur le bouton Stat Cam
         start.addEventListener('click', () => {
         
@@ -95,13 +93,15 @@ const app = {
                 catchButton.classList.remove('hidden');
                 clearButton.classList.remove('hidden');
                 stop.classList.remove('hidden');
-                // si le stream est en cours - je vérifie si il est actif et j'initialise l'affichage.
+
+                //* si le stream est en bien en cours de lecture
                 video.addEventListener("playing", () => {
+                document.getElementById('errorMsg').classList.add('hidden')
                 video.style.width ='320px';
                 video.style.heigth ='240px';
                 constrainsList.classList.remove('hidden');
                 app.browserSuportedConstraints();   
-                });//end-listener
+                });
                 
                 //*Appel functions take et reset capture canvas
                 app.takeCapture();
@@ -120,12 +120,12 @@ const app = {
                 catchButton.classList.add('hidden');
                 clearButton.classList.add('hidden');
                 canvas.classList.add('hidden');
-                });//end listener
+                });
 
-            })// end .then stream
-                .catch(function(error) {
-                    app.dislayError(error);
-                });// end catch errors
+            })
+                .catch(function(err) {
+                    app.dislayError(' Erreur dans camStreamer ' + err.name + ": " + err.message);
+                });
         });
 
     },
@@ -150,11 +150,14 @@ const app = {
     
     // initialisation du template d'affichage des messages d'erreur.
     dislayError: function(error) {
-        //si on rentre ici c'est qu'il y a eu un clic donc je vérouille la colonne de liste des contraintes
+        //si on rentre ici c'est qu'il y a une erreur
         document.getElementById('constraintList').classList.add('hidden')
+        document.getElementById('select').classList.remove('hidden')
+        document.getElementById('start').classList.remove('hidden')
         //je place une balise dans le code html pour afficher les messsages d'erreurs.
-        let errorElement = document.querySelector('#errorMsg');
-        errorElement.innerHTML += '<p> je suis dans display error'  + error + '</p>';
+        let errorElement = document.getElementById('errorMsg');
+        errorElement.classList.remove('hidden');
+        errorElement.innerHTML += '<p>' + error + '</p>';
     },
     
     // faire une capture
